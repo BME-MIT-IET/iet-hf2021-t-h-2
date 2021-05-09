a40c3f1a357ac6064614a1fc5069b70097d4a73d
keve98 <61545736+keve98@users.noreply.github.com>
2021-05-09 13:31:49 +0200

    Modifiers reordered

--- src/com/complexible/common/csv/CSV2RDF.java
+++ src/com/complexible/common/csv/CSV2RDF.java
@@ -304,7 +304,7 @@ public class CSV2RDF implements Runnable {
 		}
 	}
 
-	private static abstract class ValueProvider {
+	private abstract static class ValueProvider {
 		 private final String placeholder = UUID.randomUUID().toString();
 		 private boolean isHash;
 
@@ -380,7 +380,7 @@ public class CSV2RDF implements Runnable {
 		}
 	}
 
-	private static abstract class TemplateValueGenerator<V extends Value> implements ValueGenerator<V> {
+	private abstract static class TemplateValueGenerator<V extends Value> implements ValueGenerator<V> {
 		private final String template;
 		private final ValueProvider[] providers;
 
